6f8c47a40aa5506ab75e88be4e6985c48069f507
raingaia <raingaia.media@gmail.com>
2025-09-11 10:46:32 +0300

    Create page.tsx

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <h1>RGZTEC Marketplace</h1>
+      <p>Welcome! See the products on the <a href="/products">products</a> page.</p>
+    </>
+  );
+}
+
