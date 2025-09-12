c1e759e827fabaa371d49f123666fe1a00874198
raingaia <raingaia.media@gmail.com>
2025-09-12 17:20:06 +0300

    Update giftcard.module.ts

--- apps/api/src/modules/giftcard/giftcard.module.ts
+++ apps/api/src/modules/giftcard/giftcard.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GiftcardController } from './giftcard.controller';
+import { GiftcardService } from './giftcard.service';
 
 @Module({
   controllers: [GiftcardController],
+  providers: [GiftcardService],
 })
 export class GiftcardModule {}
-
