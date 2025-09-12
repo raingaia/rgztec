0bc5a170a83713cfdcec2c2d7daa2d1647d5211f
raingaia <raingaia.media@gmail.com>
2025-09-12 15:13:59 +0300

    Update health.controller.ts

--- apps/api/src/modules/health/health.controller.ts
+++ apps/api/src/modules/health/health.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 
-@Controller('health')
+@Controller("health")
 export class HealthController {
   @Get() ok() {
     return { ok: true, ts: new Date().toISOString() };
