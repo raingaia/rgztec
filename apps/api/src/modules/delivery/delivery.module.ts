e5919cb826d00fbd8aea0771ecb47d0b7b3e76c7
raingaia <raingaia.media@gmail.com>
2025-09-12 18:33:01 +0300

    Create delivery.module.ts

---
+++ apps/api/src/modules/delivery/delivery.module.ts
@@ -0,0 +1,7 @@
+// apps/api/src/modules/delivery/delivery.module.ts
+import { Module } from '@nestjs/common';
+import { DeliveryService } from './delivery.service';
+import { DeliveryController } from './delivery.controller';
+
+@Module({ controllers: [DeliveryController], providers: [DeliveryService], exports: [DeliveryService] })
+export class DeliveryModule {}
