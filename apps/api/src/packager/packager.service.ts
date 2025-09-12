04cb46310174768be759b58e0f95254340c5e669
raingaia <raingaia.media@gmail.com>
2025-09-12 18:26:47 +0300

    Delete apps/api/src/packager directory

--- apps/api/src/packager/packager.service.ts
+++
@@ -1,9 +0,0 @@
-// apps/api/src/modules/packager/packager.service.ts
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PackagerService {
-  ping() {
-    return { ok: true, ts: new Date().toISOString() };
-  }
-}
