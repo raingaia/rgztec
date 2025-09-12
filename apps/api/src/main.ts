74ed21ee3cb8b355aedab30ccd0af320de7af2a3
raingaia <raingaia.media@gmail.com>
2025-09-12 15:09:35 +0300

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,11 @@
-import 'reflect-metadata';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import "reflect-metadata";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
-  console.log('API ready -> http://localhost:4000');
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const port = Number(process.env.PORT ?? 4000);
+  await app.listen(port, "0.0.0.0");
+  console.log(`API ready -> http://localhost:${port}`);
 }
 bootstrap();
