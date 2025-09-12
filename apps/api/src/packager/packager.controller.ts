0b11aea61e7d0e1783d1c827e585109437ba86c2
raingaia <raingaia.media@gmail.com>
2025-09-12 18:19:13 +0300

    Create packager.controller.ts

---
+++ apps/api/src/packager/packager.controller.ts
@@ -0,0 +1,13 @@
+// apps/api/src/modules/packager/packager.controller.ts
+import { Controller, Get } from '@nestjs/common';
+import { PackagerService } from './packager.service';
+
+@Controller('packager')
+export class PackagerController {
+  constructor(private readonly svc: PackagerService) {}
+
+  @Get('health')
+  health() {
+    return this.svc.ping();
+  }
+}
