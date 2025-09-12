36c22d305ae7ac4a777f83361ed283e7eda3b635
raingaia <raingaia.media@gmail.com>
2025-09-12 15:03:33 +0300

    Create page.tsx

---
+++ apps/web/app/gift-cards/page.tsx
@@ -0,0 +1,12 @@
+export default function GiftCards() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Gift Cards</h1>
+      <ul>
+        <li><a href="/gift-cards/buy">Buy</a></li>
+        <li><a href="/gift-cards/redeem">Redeem</a></li>
+        <li><a href="/gift-cards/check">Check</a></li>
+      </ul>
+    </div>
+  );
+}
