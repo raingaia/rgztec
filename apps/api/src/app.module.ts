6034e56557e0fdb52c8af376b4470777e1ea972c
raingaia <raingaia.media@gmail.com>
2025-09-12 17:42:52 +0300

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
-import { HealthModule } from './modules/health/health.module';
-import { GiftcardModule } from './modules/giftcard/giftcard.module';
+import { Module } from "@nestjs/common";
+import { LicensingModule } from "./modules/licensing/licensing.module";
+import { GiftcardModule } from "./modules/giftcard/giftcard.module";
+import { PackagerModule } from "./modules/packager/packager.module";
+import { IntegrityModule } from "./modules/integrity/integrity.module";
+import { DeliveryModule } from "./modules/delivery/delivery.module";
 
 @Module({
-  imports: [HealthModule, GiftcardModule],
+  imports: [
+    LicensingModule,
+    GiftcardModule,
+    PackagerModule,
+    IntegrityModule,
+    DeliveryModule
+  ]
 })
 export class AppModule {}
 
