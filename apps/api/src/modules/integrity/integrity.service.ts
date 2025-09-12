1b035dfbfe914271fa014aa92396cd6c8ff9ef21
raingaia <raingaia.media@gmail.com>
2025-09-12 18:57:47 +0300

    Update integrity.service.ts

--- apps/api/src/modules/integrity/integrity.service.ts
+++ apps/api/src/modules/integrity/integrity.service.ts
@@ -1,10 +1,9 @@
-// apps/api/src/modules/integrity/integrity.service.ts
 import { Injectable } from '@nestjs/common';
 import { createHash } from 'crypto';
 
 @Injectable()
 export class IntegrityService {
-  sha256(input: string) {
+  sha256(input: string): string {
     return createHash('sha256').update(input).digest('hex');
   }
 }
