8d4b8524da508ddc980ac81be50667661458e46a
raingaia <raingaia.media@gmail.com>
2025-09-12 15:12:15 +0300

    Update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,8 @@
-import 'reflect-metadata';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Module } from "@nestjs/common";
+import { HealthModule } from "./modules/health/health.module";
+import { GiftCardModule } from "./modules/giftcard/giftcard.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
-  console.log('API ready -> http://localhost:4000');
-}
-bootstrap();
+@Module({
+  imports: [HealthModule, GiftCardModule]
+})
+export class AppModule {}
