26a330cb1ccf554da14b09eab44e031317b4913e
raingaia <raingaia.media@gmail.com>
2025-12-23 13:43:40 +0300

    Delete apps/web/app directory

--- apps/web/app/licensing/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Licensing() {
-  return (
-    <div>
-      <h1>Licensing</h1>
-      <p>Issue / Verify / Activate via API endpoints.</p>
-    </div>
-  );
-}
--- apps/web/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <>
-      <h1>RGZTEC Marketplace</h1>
-      <p>Welcome.</p>
-    </>
-  );
-}
