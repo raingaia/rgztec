18e86796b29071e0a2ce318fe1acca01fef61509
raingaia <raingaia.media@gmail.com>
2025-12-23 15:17:50 +0300

    Delete apps/web/next.config.mjs

--- apps/web/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', 
-  distDir: 'public', // Çıktıyı doğrudan public klasöründen almasını söyleyelim
-  images: { unoptimized: true }
-};
-export default nextConfig;
