7d116349ead5060b60454349e3ede7e924345c53
raingaia <raingaia.media@gmail.com>
2026-01-01 14:52:10 +0300

    Delete apps directory

--- apps/api/config.js
+++
@@ -1,8 +0,0 @@
-// apps/api/config.js
-export const API_CONFIG = {
-    version: "1.2.0",
-    base_currency: "USD",
-    vat_rate: 0.18,
-    global_discount: 0.10, // %10 genel indirim
-    store_paths: "../data/" // Verilerin okunduğu klasör
-};
