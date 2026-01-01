ad14321e121a7bdbcfc478828b1060fc1875a8ef
raingaia <raingaia.media@gmail.com>
2026-01-01 15:55:22 +0300

    Update next.config.js

--- apps/saas/next.config.js
+++ apps/saas/next.config.js
@@ -2,14 +2,11 @@
 const nextConfig = {
   async rewrites() {
     return [
-      // UI routes
       { source: "/app", destination: "/dashboard" },
-      { source: "/app/:path*", destination: "/:path*" },
-
-      // API routes (standard)
-      { source: "/api/:path*", destination: "/api/:path*" },
+      { source: "/app/:path*", destination: "/:path*" }
     ];
-  },
+  }
 };
 
 module.exports = nextConfig;
+
