2c1572c40fe6719d0fc79abe951921207c2d1e55
raingaia <raingaia.media@gmail.com>
2026-01-02 10:28:56 +0300

    Update route.ts

--- apps/saas/app/api/search/route.ts
+++ apps/saas/app/api/search/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { buildSearchIndex } from "@/lib/search-index.builder";
+import { buildSearchIndex } from "../../../lib/search-index.builder";
 
 export const dynamic = "force-static";
 
