e7b0a17347ed6102e6f6d473dc21f226a6186393
raingaia <raingaia.media@gmail.com>
2026-01-05 12:02:51 +0300

    Delete src directory

--- src/rgz/admin/publishClient.js
+++
@@ -1,7 +0,0 @@
-import { makeAdminClient } from "./apiClient.js";
-
-export async function quickPublish({ adminKey }) {
-  const api = makeAdminClient({ adminKey });
-  const r = await api.publish();
-  return r;
-}
